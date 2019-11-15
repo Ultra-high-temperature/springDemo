2ae4134f3a08d1bb2df726aa14c7d2f625e755d2
UHT <1404119365@qq.com>
2019-11-15 09:18:51 +0800

    视频进度P41

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -1,6 +1,25 @@
 function post() {
-    var question_id=  $("input:hidden[name='question_id']").val();
+    var question_id = $("input:hidden[name='question_id']").val();
+    var content = $("#content").val();
+    $.ajax({
+        type: "POST",
+        url: "/comment",
+        contentType: 'application/json',
+        data: JSON.stringify({
+            "parentId": question_id,
+            "content": content,
+            "type": 1
+        }),
+        success: function (response) {
+            if(response.code==200){
+                $("#comment_section").hide()
+            }
+            else {
+                alert(response.code,response.message);
+            }
+            console.log(response);
+        },
+        dataType: "json"
+    });
 
-    //parent_id=$("#parent_id").val();
-    console.log(question_id);
 }
